c621bb3cbcf85dcd0208231148753ac3f39ca70c
Stanley Lai <kl034c99@gmail.com>
2019-03-30 23:03:25 +0800

    Updated home screen style and applied all changes

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -1,29 +1,125 @@
 import React, {Component} from 'react';
-import {Platform, Text, View} from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+
+import NavHeader from '../../components/NavHeader';
+
+import Api from '../../services/Api';
 
 import styles from './styles';
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { IDefineResult } from '../../interfaces/intefaces';
 
 interface Props {}
 
-export default class HomeScreen extends Component<Props> {
+interface HomeScreenState {
+    searchingWord: string;
+    searchResult: IDefineResult;
+}
+
+export default class HomeScreen extends Component<Props, any> {
     // config header
     static navigationOptions = {
-        title: 'Home Screen'
+        header: null,
     };
+
+    /**
+     * Constructor
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchingWord: '',
+            results: null
+        };
+    }
+    
+    /**
+     * Event Handlers
+     */
+    onSearchBarChangedText = (text) => {
+        this.setState({searchingWord: text});
+        this.searchDefine();
+    }
+
+    /**
+     * Actions
+     */
+    async searchDefine() {
+        try {
+            const results = this.getDefine(this.state.searchingWord);
+            this.setState({results: results});
+        } catch(e) {
+            console.log('[searchDefine] Error: ', e);
+        } finally {
+
+        }
+    }
+
+    /**
+     * Apis
+     */
+    async getDefine(word) {
+        try { 
+            const defineResponse: IDefineResult = await Api.getDefine({
+                term: word
+            });
+            console.log(defineResponse);
+            return defineResponse;
+        } catch(e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Render
+     */
+    renderWordDefinationListItem = ({item}) => {
+        console.log('[renderWordDefinationListItem] item: ', item);
+        return(
+            <View>
+                <Text>1</Text>
+            </View>
+        );
+    }
+
+
+    renderDefination = () => {
+        if(!this.state.results) {
+            return(
+                <View style={styles.noResultContainer}>
+                    <Text>No Results</Text>
+                </View>
+            );
+        } else {
+            return(
+                <FlatList
+                    data={this.state.results.list}
+                    renderItem={this.renderWordDefinationListItem}
+                />
+            );
+        }
+    }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
+                {/* Header */}
+                <NavHeader
+                    centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+                />
+
+                {/* Search Bar */}
+                <SearchBar
+                    placeholder={"Search..."}
+                    onChangeText={this.onSearchBarChangedText}
+                    value={this.state.searchingWord}
+                    containerStyle={styles.searchBarContainer}
+                    inputContainerStyle={styles.searchBarInput}
+                    round
+                />
+
+                {/* Results List */}
+                {this.renderDefination()}
             </View>
         );
     }
