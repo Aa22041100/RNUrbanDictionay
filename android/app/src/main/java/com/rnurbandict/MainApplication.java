15036030b9d1b25ef8935d8175e4cbb0a0771eb5
Stanley Lai <stanley.lai.104@gmail.com>
2019-04-13 22:03:09 +0800

    Removed react-native-sound, installed react-native-sound-player for streaming audio from URL

--- android/app/src/main/java/com/rnurbandict/MainApplication.java
+++ android/app/src/main/java/com/rnurbandict/MainApplication.java
@@ -3,7 +3,7 @@ package com.rnurbandict;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.zmxv.RNSound.RNSoundPackage;
+import com.johnsonsu.rnsoundplayer.RNSoundPlayerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSoundPackage(),
+            new RNSoundPlayerPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );
