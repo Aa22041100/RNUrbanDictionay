26f7d357787e1e92ce0d74f717d7e2c35b4be7a3
Stanley Lai <stanley.lai.104@gmail.com>
2019-04-13 22:26:50 +0800

    Added a common shared BaseStyle

---
+++ src/themes/BaseStyles.tsx
@@ -0,0 +1,24 @@
+export const colors = {
+    primary: '#ff0000',
+    secondary: '#00ff00',
+    highlighted: '#0000ff',
+    muted: '#999',
+}
+
+// from https://material-ui.com/layout/grid/
+export const padding = {
+    xs: 8,
+    sm: 16,
+    md: 24,
+    lg: 32,
+    xl: 40
+}
+
+// from https://material-ui.com/layout/grid/
+export const margin = {
+    xs: 8,
+    sm: 16,
+    md: 24,
+    lg: 32,
+    xl: 40
+}
